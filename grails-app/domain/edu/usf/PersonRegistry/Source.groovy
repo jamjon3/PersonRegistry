3d327429c8b0250cea378d19d1c608da04ea78c6
James Jones <jamjon3@gmail.com>
2013-10-03 13:20:17 -0400

    Created Source domain class to handle source of record names

---
+++ grails-app/domain/edu/usf/PersonRegistry/Source.groovy
@@ -0,0 +1,17 @@
+package edu.usf.PersonRegistry
+
+class Source {
+    String name
+    static hasMany = [identifiers:Identifier]
+    static constraints = {
+        name(   
+                blank: false,
+                nullable: false,
+                size: 3..255,
+                unique: true,
+                //Custom constraint - only allow upper, lower, digits, dash and underscore
+                validator: { val, obj -> val ==~ /[A-Za-z0-9_.-]+/ }
+        )
+    }
+    
+}
