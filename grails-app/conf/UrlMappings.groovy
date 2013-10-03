409bac2d86a5f5c9a2390129e13e3ddc128afd57
James Jones <jamjon3@gmail.com>
2013-10-03 12:49:48 -0400

    Updated REST configuration to handle import and exporting of genealogical data

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,12 +1,16 @@
 class UrlMappings {
 
 	static mappings = {
-                "/person"(controller:"ruleSet",parseRequest: true){ 
+                "/person"(controller:"person",parseRequest: true){ 
                     action = [GET:"error", PUT:"addPerson", DELETE:"error", POST:"error"] 
                 } 
-                "/identifierType/$name"(controller:"ruleSet",parseRequest: true){ 
+                "/identifierType/$name"(controller:"identifier",parseRequest: true){ 
                     action = [GET:"getIdentifierType", PUT:"addIdentifierType", DELETE:"error", POST:"error"] 
                 } 
+                "/genealogy/data/download"(controller:"genealogy",action: "exportGenealogy",parseRequest: true)
+                "/genealogy/data/upload"(controller:"genealogy",parseRequest: true){ 
+                    action = [GET:"error", PUT:"importGenealogy", DELETE:"error", POST:"error"] 
+                }
 
 		"/"(view:"/index")
 		"500"(view:'/error')
