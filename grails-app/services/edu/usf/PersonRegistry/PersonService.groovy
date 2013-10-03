164d53d38f2f17722f3a35009a96e6f5cdc61dd6
James Jones <jamjon3@gmail.com>
2013-10-03 15:26:15 -0400

    Simplified the "person" output to the person UUID only

--- grails-app/services/edu/usf/PersonRegistry/PersonService.groovy
+++ grails-app/services/edu/usf/PersonRegistry/PersonService.groovy
@@ -7,9 +7,9 @@ class PersonService {
     def addPerson() {
         def person = new Person()
         if(!person.save(failOnError:false, flush: true, insert: true, validate: true)) {
-            return [ error : "Name value '${person.errors.fieldError.rejectedValue}' rejected" ]
+            return [ error : "'${person.errors.fieldError.field}' value '${person.errors.fieldError.rejectedValue}' rejected" ]
         } else {
-            return [ person: person ]
+            return [ personId: person.id ]
         }        
     }
 }
