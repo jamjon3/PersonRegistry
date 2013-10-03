689dd27d5d1998ec7261f187e8e23761e0997e81
James Jones <jamjon3@gmail.com>
2013-10-03 12:48:52 -0400

    Created Genealogy service class to handle import and exporting of genealogical data

---
+++ grails-app/services/edu/usf/PersonRegistry/GenealogyService.groovy
@@ -0,0 +1,50 @@
+package edu.usf.PersonRegistry
+
+class GenealogyService {
+
+    def exportGenealogy() {
+        return [
+            genealogy: Genealogy.createCriteria().list(sort: 'realname', order:'desc') {
+                resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+                projections {
+                    property('realname', 'realname')
+                    property('nickname', 'nickname')
+                    property('relationship','relationship')
+                }
+            }        
+        ]
+    }
+    def importGenealogy(genealogy) {
+        if(!!genealogy) {
+            if('genealogy' in genealogy) {
+                def summary = [
+                    existing: 0,
+                    added: 0,
+                    errored: 0
+                ]
+                genealogy.genealogy.each { g ->
+                    if(g.relationship in Genealogy.constraints.relationship.inList) {
+                        def gexists = Genealogy.find(g as Genealogy)
+                        if(!!!gexists) {
+                            gexists = g as Genealogy
+                            if(!gexists.save(failOnError:false, flush: true, insert: true, validate: true)) {
+                                log.warn "'${gexists.errors.fieldError.field}' value '${gexists.errors.fieldError.rejectedValue}' rejected" 
+                                summary.errored++
+                            } else {
+                                summary.added++
+                            }                            
+                        } else {
+                            summary.existing++
+                        }
+                    } else {
+                        summary.errored++
+                        log.warn "relationship type is invalid"
+                    }
+                }
+                return summary
+            }
+            return [ error: "Your data doesn't contain any entries"]
+        }
+        return [ error: "You must provide Geneological data for the import"]
+    }
+}
